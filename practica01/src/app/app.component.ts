fd99e23da933370c190af7a4d55b87dcbb1f8627
unknown <xuchen002@gmail.com>
2021-03-05 15:21:33 +0100

    first commit

---
+++ practica01/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'practica01';
+  getTitle(){
+    return this.title;
+  }
+}
